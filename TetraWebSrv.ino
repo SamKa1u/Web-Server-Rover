5e937906be82c3af30403392c55f09fa2d2dff2a
Samuel Kalu <samkalu@ttu.edu>
2025-04-19 02:59:55 -0500

    Add files via upload

---
+++ TetraWebSrv.ino
@@ -0,0 +1,143 @@
+#include <ESP8266WiFi.h>
+#include <ESPAsyncWebServer.h>
+#include <LittleFS.h>
+
+#define Pin0 D1
+#define Pin1 D2
+#define Pin2 D5
+#define Pin3 D7
+
+
+AsyncWebServer server(80); //server port 80
+
+void notFound(AsyncWebServerRequest *request)
+{
+  request->send(404, "text/plain", "Not Found");
+}
+
+
+void setup(void) 
+{    
+  Serial.begin(115200);
+  if(!LittleFS.begin()){
+    Serial.println("An Error has occurred while mounting LittleFS");
+}
+  delay(1);
+  Serial.println("Listing files:");
+  Dir dir = LittleFS.openDir("/");
+  while (dir.next()) {
+    Serial.print("File: ");
+    Serial.println(dir.fileName());
+}
+  pinMode(Pin0,OUTPUT);
+  pinMode(Pin1,OUTPUT);
+  pinMode(Pin2,OUTPUT);
+  pinMode(Pin3,OUTPUT);
+
+  WiFi.softAP("TetraWEBSrv", "");    //defines access point name and password
+  Serial.print("softap ");
+  Serial.println(WiFi.softAPIP());  //print IP of access point to serial monitor 
+  
+//Main page
+  server.on("/home",HTTP_GET, [](AsyncWebServerRequest *request)
+  {
+    digitalWrite(Pin0, LOW);
+    digitalWrite(Pin1, LOW);
+    digitalWrite(Pin2, LOW);
+    digitalWrite(Pin3, LOW);
+    request->send(LittleFS, "/tetraWebSrv.html", "text/html");
+  });
+
+//formating and images
+  server.on("/index.css",HTTP_GET, [](AsyncWebServerRequest *request)
+  {
+    request->send(LittleFS, "/index.css", "text/css");
+  });
+  server.on("/keyboard_key_up.png",HTTP_GET, [](AsyncWebServerRequest *request)
+  {
+    request->send(LittleFS, "/keyboard_key_up.png", "text/plain");
+  });
+  server.on("/keyboard_key_left.png",HTTP_GET, [](AsyncWebServerRequest *request)
+  {
+    request->send(LittleFS, "/keyboard_key_left.png", "text/plain");
+  });
+  server.on("/keyboard_key_right.png",HTTP_GET, [](AsyncWebServerRequest *request)
+  {
+    request->send(LittleFS, "/keyboard_key_right.png", "text/plain");
+  });
+  server.on("/keyboard_key_down.png",HTTP_GET, [](AsyncWebServerRequest *request)
+  {
+    request->send(LittleFS, "/keyboard_key_down.png", "text/plain");
+  });
+  server.on("/stop.png",HTTP_GET, [](AsyncWebServerRequest *request)
+  {
+    request->send(LittleFS, "/stop.png", "text/plain");
+  });
+  server.on("/autopilot.png",HTTP_GET, [](AsyncWebServerRequest *request)
+  {
+    request->send(LittleFS, "/autopilot.png", "text/plain");
+  });
+//Movement control
+  //stop
+  server.on("/stop", HTTP_GET, [](AsyncWebServerRequest *request)
+  {
+    digitalWrite(Pin0, LOW);
+    digitalWrite(Pin1, LOW);
+    digitalWrite(Pin2, LOW);
+    digitalWrite(Pin3, LOW);
+    request->send(LittleFS, "/tetraWebSrv.html", "text/html");
+  });
+  //backwards
+  server.on("/back", HTTP_GET, [](AsyncWebServerRequest *request)
+  {
+    digitalWrite(Pin0, LOW);
+    digitalWrite(Pin1, LOW);
+    digitalWrite(Pin2, LOW);
+    digitalWrite(Pin3, HIGH);
+    request->send(LittleFS, "/tetraWebSrv.html", "text/html");
+  });
+  //Turn Left
+  server.on("/left", HTTP_GET, [](AsyncWebServerRequest *request)
+  {
+    digitalWrite(Pin0, LOW);
+    digitalWrite(Pin1, LOW);
+    digitalWrite(Pin2, HIGH);
+    digitalWrite(Pin3, LOW);
+    request->send(LittleFS, "/tetraWebSrv.html", "text/html");
+  });
+  //Turn right
+  server.on("/right", HTTP_GET, [](AsyncWebServerRequest *request)
+  {
+    digitalWrite(Pin0, LOW);
+    digitalWrite(Pin1, LOW);
+    digitalWrite(Pin2, HIGH);
+    digitalWrite(Pin3, HIGH);
+    request->send(LittleFS, "/tetraWebSrv.html", "text/html");
+  });
+  //forwards
+  server.on("/forward", HTTP_GET, [](AsyncWebServerRequest *request)
+  {
+    digitalWrite(Pin0, LOW);
+    digitalWrite(Pin1, HIGH);
+    digitalWrite(Pin2, LOW);
+    digitalWrite(Pin3, LOW);
+    request->send(LittleFS, "/tetraWebSrv.html", "text/html");
+  });
+  //sentry mode
+  server.on("/auto", HTTP_GET, [](AsyncWebServerRequest *request)
+  {
+    digitalWrite(Pin0, LOW);
+    digitalWrite(Pin1, HIGH);
+    digitalWrite(Pin2, LOW);
+    digitalWrite(Pin3, HIGH);
+    request->send(LittleFS, "/tetraWebSrv.html", "text/html");
+  });
+
+  server.onNotFound(notFound);
+  server.begin();
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+}
---
+++ TetraWebsrvControl.ino
@@ -0,0 +1,199 @@
+// File:          TetraAvoidCollision.c
+// Date:
+// Description:
+// Author:
+#include <Servo.h>
+
+//Motor pins
+#define M1A 8
+#define M1B 9
+#define M2A 10
+#define M2B 11
+#define Serv0 15
+//Control pins 
+#define Pin0 28
+#define Pin1 7
+#define Pin2 26
+#define Pin3 6
+//Infrared pins
+#define IRL 4
+#define IRR 16
+
+//Define variables
+int msg, IRL_sig, IRR_sig;
+char obs;
+
+void setup() {
+  Serial.begin(9600);
+//initialize Motors as output
+  pinMode(M1A,OUTPUT);
+  pinMode(M2A,OUTPUT);
+  pinMode(M1B,OUTPUT);
+  pinMode(M2B,OUTPUT);
+//initialize Control pins
+  pinMode(Pin0,INPUT);
+  pinMode(Pin1,INPUT);
+  pinMode(Pin2,INPUT);
+  pinMode(Pin3,INPUT);
+//initialize Infrared pins as inputs
+  pinMode(IRL,INPUT);
+  pinMode(IRR,INPUT);
+
+}
+
+int command() {
+  bool pin0 = digitalRead(Pin0);
+  bool pin1 = digitalRead(Pin1);
+  bool pin2 = digitalRead(Pin2);
+  bool pin3 = digitalRead(Pin3);
+
+  if (pin0 == 0 && pin1 == 0 && pin2 == 0 && pin3 == 1 ){
+    return 2;
+  }
+  else if (pin0 == 0 && pin1 == 0 && pin2 == 1 && pin3 == 0 ){
+    return 3;
+  }
+  else if (pin0 == 0 && pin1 == 0 && pin2 == 1 && pin3 == 1 ){
+    return 4;
+  }
+  else if (pin0 == 0 && pin1 == 1 && pin2 == 0 && pin3 == 0 ){
+    return 5;
+  }
+  else if (pin0 == 0 && pin1 == 1 && pin2 == 0 && pin3 == 1 ){
+    return 6;
+  }
+  else if (pin0 == 0 && pin1 == 1 && pin2 == 1 && pin3 == 0 ){
+    return 7;
+  }
+  else if (pin0 == 0 && pin1 == 1 && pin2 == 1 && pin3 == 1 ){
+    return 8;
+  }
+  else if (pin0 == 1 && pin1 == 0 && pin2 == 0 && pin3 == 0 ){
+    return 9;
+  }
+  else if (pin0 == 1 && pin1 == 0 && pin2 == 0 && pin3 == 1 ){
+    return 10;
+  }
+  else if (pin0 == 1 && pin1 == 0 && pin2 == 1 && pin3 == 0 ){
+    return 11;
+  }
+  else if (pin0 == 1 && pin1 == 0 && pin2 == 1 && pin3 == 1 ){
+    return 12;
+  }
+  else if (pin0 == 1 && pin1 == 1 && pin2 == 0 && pin3 == 0 ){
+    return 13;
+  }
+  else if (pin0 == 1 && pin1 == 1 && pin2 == 0 && pin3 == 1 ){
+    return 14;
+  }
+  else if (pin0 == 1 && pin1 == 1 && pin2 == 1 && pin3 == 0 ){
+    return 15;
+  }
+  else if (pin0 == 1 && pin1 == 1 && pin2 == 1 && pin3 == 1 ){
+    return 16;
+  }
+  else {
+    return 1;
+  }
+}
+
+void sentry() {
+  IRR_sig = !digitalRead(IRL);
+  IRL_sig = !digitalRead(IRR);
+
+  // Process sensor data here.
+  if (IRL_sig && !IRR_sig ) {
+    obs = 'l';
+  }
+  else if (IRR_sig && !IRL_sig){
+    obs = 'r';
+  }
+  else if (IRL_sig && IRR_sig){
+    obs = 'f';
+  }
+  else {
+    obs = 'n';
+  }
+  switch (obs) {
+      case 'n':
+        //forwards
+      forwards();
+      break;
+      case 'l':
+        //turn right
+      left();
+      break;
+      case 'r':
+        //turn left
+      right();
+      break;
+      case 'f':
+        //reverse
+      reverse();
+      break;
+    }    
+}
+
+void reverse(){
+  digitalWrite(M1A, HIGH);
+  digitalWrite(M1B, LOW);
+  digitalWrite(M2A, LOW);
+  digitalWrite(M2B, HIGH);
+}
+void left(){
+  digitalWrite(M1A, HIGH);
+  digitalWrite(M1B, LOW);
+  digitalWrite(M2A, HIGH);
+  digitalWrite(M2B, LOW);
+}
+void right(){
+  digitalWrite(M1A, LOW);
+  digitalWrite(M1B, HIGH);
+  digitalWrite(M2A, LOW);
+  digitalWrite(M2B, HIGH);
+}
+void forwards(){
+  digitalWrite(M1A, LOW);
+  digitalWrite(M1B, HIGH);
+  digitalWrite(M2A, HIGH);
+  digitalWrite(M2B, LOW);
+}
+void stop(){
+  digitalWrite(M1A, LOW);
+  digitalWrite(M1B, LOW);
+  digitalWrite(M2A, LOW);
+  digitalWrite(M2B, LOW);
+}
+void loop() {
+  // Read the control pins:
+  msg = command();
+  Serial.println(msg);
+  //actuator commands
+    switch (msg) {
+      case 2:
+        //reverse
+        reverse();
+        break;
+      case 3:
+        //turn left
+        left();
+        break;
+      case 4:
+        //turn right
+        right();
+        break;
+      case 5:
+        //forwards
+        forwards();
+        break;
+      case 6:
+        //auto
+        sentry();
+        break;
+      default:
+        //stop
+        stop();
+        break;
+    }
+}
+
